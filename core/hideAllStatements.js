d8c754eab3de00766b23298c5698c63fab3879b8
gness1804 <graham.nessler@gmail.com>
2021-07-20 21:00:52 -0500

    👌 IMPROVE: don't hide already hidden statements

--- core/hideAllStatements.js
+++ core/hideAllStatements.js
@@ -13,19 +13,26 @@ const hideStatementsInFile = async (file) => {
   const resultsOneLine = data.match(regexDiffReplacer);
   const allResults = data.match(regexDiffShort);
 
+  let hiddenCount = 0;
+
   if (resultsOneLine && resultsOneLine.length) {
     let text = await readFile(file, 'utf8');
 
     for (let res of resultsOneLine) {
       res = res.replace(/\+/, '');
-      text = text.replace(res, `//${res}`);
+      // we don't want to hide statements that are already hidden
+      if (!res.match(/^\/\//)) {
+        text = text.replace(res, `//${res}`);
+        hiddenCount++;
+      }
     }
 
     await writeFile(file, text, 'utf8');
-    alert({
-      type: 'success',
-      msg: `Successfully hid ${resultsOneLine.length} introduced console statements from ${file}`,
-    });
+    hiddenCount > 0 &&
+      alert({
+        type: 'success',
+        msg: `Successfully hid ${hiddenCount} introduced console statements from ${file}`,
+      });
 
     if (allResults.length > resultsOneLine.length) {
       alert({
